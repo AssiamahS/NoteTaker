d4e19517fc44829b4808c92aa1580502d1411017
Sylvester Assiamah <sylvesterassiamahpm@gmail.com>
2024-06-03 23:32:28 -0400

    init commit

--- Develop/server.js
+++ Develop/server.js
@@ -1,76 +1,67 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-
 const app = express();
-const PORT = process.env.PORT || 1010;
+const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Routes
 // HTML Routes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // API Routes
+
+
+//GET route for notes
 app.get('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('An error occurred while reading the notes.');
-        }
-        res.json(JSON.parse(data));
-    });
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
 });
 
+
+// POST route for  notes
 app.post('/api/notes', (req, res) => {
-    const newNote = { id: uuidv4(), ...req.body };
-    fs.readFile('db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('An error occurred while reading the notes.');
-        }
-        const notes = JSON.parse(data);
-        notes.push(newNote);
-        fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('An error occurred while saving the note.');
-            }
-            res.json(newNote);
-        });
+  const newNote = { id: uuidv4(), ...req.body };
+
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes, null, 2), (err) => {
+      if (err) throw err;
+      res.json(newNote);
     });
+  });
 });
 
-// Bonus: Delete Route
+//  DELETE route for notes
 app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    fs.readFile('db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('An error occurred while reading the notes.');
-        }
-        let notes = JSON.parse(data);
-        notes = notes.filter(note => note.id !== noteId);
-        fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('An error occurred while deleting the note.');
-            }
-            res.json({ message: 'Note deleted' });
-        });
+  const noteId = req.params.id;
+
+  fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const newNotes = notes.filter(note => note.id !== noteId);
+
+    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(newNotes, null, 2), (err) => {
+      if (err) throw err;
+      res.json({ message: `Note ${noteId} deleted` });
     });
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
